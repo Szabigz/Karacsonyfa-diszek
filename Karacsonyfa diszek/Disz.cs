349481ec3e4bb219259927bf8a3a0a959c5598e2
Gózon Szabolcs <gozon.szabolcs.daniel.21i@egressy.info>
2024-12-04 11:32:10 +0100

    Add project files.

---
+++ Karacsonyfa diszek/Disz.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karacsonyfa_diszek
+{
+    class Disz
+    {
+        public string name { get; set; }
+        public int price { get; set; }
+        public int stock { get; set; }
+    }
+}
